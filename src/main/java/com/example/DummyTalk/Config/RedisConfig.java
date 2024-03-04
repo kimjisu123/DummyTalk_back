3285e8c829349e35dcd18fc1c7f0424a8f9a5b47
DESKTOP-A9G14KA\Admin <dkljjh10@gmail.com>
2024-03-05 01:36:06 +0900

    Redis 기본 설정

---
+++ src/main/java/com/example/DummyTalk/Config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.example.DummyTalk.Config;
+
+
+import com.google.api.client.util.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+
+        return new LettuceConnectionFactory(host, port);
+        // 자바에서 사용하는 Redis Client는 크게 2가지가 있음 (Jedis, Lettuce)
+        // 스프링부트 2.0 이후로는 기본 클라이언트로 Lettuce가 사용이 되기에 사용을 함
+    }
+}
